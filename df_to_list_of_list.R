696008da06c5a8c0d912f68e55c15642601fa005
Claudia Bartolini <clabar@arsanita.toscana.it>
2022-05-31 15:11:26 +0200

    added others coding system

--- df_to_list_of_list.R
+++ df_to_list_of_list.R
@@ -31,9 +31,9 @@ df_to_list_of_list <- function(x, code_col = "code", concepts_col = "event_abbre
   
   if ("tags" %in% colnames(x)) {
     x <- x[!is.na(tags) & tags != "", (concepts_col) := paste(get(concepts_col), get(type_col), tags, sep = "_")]
+    y <- y[, (concepts_col) := paste(get(concepts_col), get(type_col), sep = "_")]
   }
   
-  y <- y[, (concepts_col) := paste(get(concepts_col), get(type_col), sep = "_")]
   
   x <- x[, .SD, .SDcols = c(code_col, "coding_system", concepts_col)]
   y <- y[, .SD, .SDcols = c(code_col, "coding_system", concepts_col)]
@@ -44,13 +44,19 @@ df_to_list_of_list <- function(x, code_col = "code", concepts_col = "event_abbre
     next
   } else if (tolower(codying_system_recode) == "auto") {
     x[, coding_system := data.table::fcase(
-      coding_system %in% c("ICD10", "ICD10CM", "ICD10DA", "ICD10GM"), "ICD10",
+      coding_system %in% c("ICD10", "ICD10CM", "ICD10DA"), "ICD10",
+      coding_system %in% c("ICD10GM"), "ICD10GM",
       coding_system %in% c("Free_text"), "Free_text",
       coding_system %in% c("ICD9CM", "MTHICD9"), "ICD9",
       coding_system %in% c("ICPC"), "ICPC",
       coding_system %in% c("ICPC2P", "ICPC2EENG"), "ICPC2P",
       coding_system %in% c("RCD2", "RCD"), "READ",
-      coding_system %in% c("MEDCODEID", "SCTSPA", "SNOMEDCT_US", "SPA_EXT", "SNM"), "SNOMED"
+      coding_system %in% c("MEDCODEID", "SCTSPA", "SNOMEDCT_US", "SPA_EXT", "SNM"), "SNOMED",
+      coding_system %in% c("OPS"), "OPS",
+      coding_system %in% c("EBM"), "EBM",
+      coding_system %in% c("CCAM"), "CCAM",
+      coding_system %in% c("CNAM"), "CNAM",
+      coding_system %in% c("NABM"), "NABM"
     )]
   } else {
     x[codying_system_recode, on = c("coding_system" = colnames(codying_system_recode)[[1]]),
